8410ce46f6b585175fd2a5f6978c500ca5fafab7
Mina97 <tkarnrwl78627862@gmail.com>
2021-01-08 11:38:32 +0900

    starter updated

--- system_variable_set.sh
+++ system_variable_set.sh
@@ -1,11 +1,3 @@
-ROOT=~/Desktop
-AppName=myNewApp
-MacName=mac
-cd $ROOT &&
-react-native init $AppName --version react-native@0.60.0 &&
-cd $ROOT/$AppName/android &&
-echo sdk.dir = /Users/$MacName/Library/Android/sdk > local.properties &&
-cd .. &&
 export ANDROID_SDK=/Users/$MacName/Library/Android/sdk &&
 echo 'export PATH=/Users/$MacName/Library/Android/sdk/platform-tools:/Users/mac/.nvm/versions/node/v12.13.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/mac/.nvm/versions/node/v12.13.0/bin:/Users/mac/.rvm/bin' >>~/.bash_profile &&
 source ~/.bash_profile
